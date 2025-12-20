dbb82049217fa9fb6e5972fa1bfe9393753e72dd
anonymous316somil <150796783+anonymous316somil@users.noreply.github.com>
2025-12-20 19:56:21 +0000

    string conversion confusion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,29 +1,67 @@
 // let score = 33
 // let score = "33abc"
-// let score = null 
-// let score = undefined 
+// let score = null
+// let score = undefined
 // let score = true
-let score = "anshu"
+let score = "anshu";
 
-// console.log(typeof score); 
-// console.log(typeof(score)); 
+// console.log(typeof score);
+// console.log(typeof(score));
 
-let valueInNumber = Number(score); 
-// console.log(typeof(valueInNumber)); 
-// console.log(valueInNumber); 
+let valueInNumber = Number(score);
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
 
 // "33" => 33
 // "33abc" => NaN
 // true => 1; false => 0
 
 // let isLoggedIn = 1
-// let isLoggedIn = ""  => false 
-// let isLoggedIn = "somil" => true
-// let booleanIsLoggedIn = Boolean(isLoggedIn)
-// console.log(booleanIsLoggedIn)
+// let isLoggedIn = ""  => false
+// let isLoggedIn = "somil" => true 
+// let booleanIsLoggedIn = Boolean(isLoggedIn) 
+// console.log(booleanIsLoggedIn) 
+ 
+let someNumber = 33; 
 
-let someNumber = 33
+let stringNumber = String(someNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
 
-let stringNumber = String(someNumber); 
-console.log(stringNumber);  
-console.log(typeof stringNumber);  
+// ******************************************** Operations *********************************************
+let value = 3;
+let negValue = -value;
+// console.log(value);
+// console.log(negValue);
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 ** 2);
+// console.log(2 / 2);
+// console.log(2 % 2);
+
+let str1 = "hello"
+let str2 = " coffee"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1"+2); 
+// console.log(1+"2"); 
+// console.log("1"+"2"); 
+// console.log(1+2);
+// console.log("1"+2+2); 
+// console.log(1+2+"2");
+
+// console.log((3+4) * 5 % 3);
+
+// console.log(+true);
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter)
+gameCounter--
+console.log(gameCounter);
+
