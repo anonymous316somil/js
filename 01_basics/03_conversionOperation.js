78fa7f03d2b9d30bf9850429de066cf36b5fa775
anonymous316somil <150796783+anonymous316somil@users.noreply.github.com>
2025-12-20 19:42:19 +0000

    Conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,29 @@
+// let score = 33
+// let score = "33abc"
+// let score = null 
+// let score = undefined 
+// let score = true
+let score = "anshu"
+
+// console.log(typeof score); 
+// console.log(typeof(score)); 
+
+let valueInNumber = Number(score); 
+// console.log(typeof(valueInNumber)); 
+// console.log(valueInNumber); 
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+// let isLoggedIn = 1
+// let isLoggedIn = ""  => false 
+// let isLoggedIn = "somil" => true
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+let someNumber = 33
+
+let stringNumber = String(someNumber); 
+console.log(stringNumber);  
+console.log(typeof stringNumber);  
